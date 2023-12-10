4979f08537d034495aee37dd7c02dd3028fe99e7
Amirali Ahmadi <amiraliahmadi8142@yahoo.com>
2023-12-10 06:13:54 -0800

    third commit

--- components/context test/MyContext.js
+++ components/context test/MyContext.js
@@ -2,4 +2,4 @@ import React from "react";
 
 const myContext = React.createContext()
 export const myProvider = myContext.Provider
-export const myConsumer = myContext.Consumer
+export const myConsumer = myContext.Consumer 
